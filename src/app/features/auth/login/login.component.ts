5566ad0055ece40730f5973c87d5ea0c2062b961
Rutvik-Alandikar <rutvik.alandikar@accolitedigital.com>
2022-03-22 13:12:08 +0530

    Added Node Modules

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+  isLoggedIn = false;
+  form: any = {
+    Username: null,
+    Password: null
+  };
+  public form1: FormGroup = new FormGroup({
+    userName: new FormControl(''),
+  });
+  getUsername(){
+    alert("Username Entered: " + this.form.Username + "  Password Entered: " +this.form.Password );
+   // alert(this.form.Password);
+
+  }
+  verify(){
+    if(this.form.Username=="Rutvik" && this.form.Password=="Password")
+    alert("Login Succesful");
+    else
+    alert("Login failed!!!!!");
+  }
+
 
   constructor() { }
 
